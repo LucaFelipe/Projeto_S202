09d691228559581fa80e5f16059d0c31d7d76eb2
LucaFelipe <luca.felipe@ges.inatel.br>
2023-11-11 20:17:38 -0300

    classes criadas

---
+++ classe/cliente.py
@@ -0,0 +1,4 @@
+class Cliente:
+    def __init__(self, nome,quantidade_item):
+        self.nome = nome
+        self.quantidade_item = quantidade_item
---
+++ classe/produto.py
@@ -0,0 +1,5 @@
+class Produto:
+    def __init__(self, nome, preco, data_validade):
+        self.nome = nome
+        self.preco = preco
+        self.data_validade = data_validade
